0764800647bccf5b47edf1ac13e866ff6805b33f
MoriczJanos113C <Sh2rm56kL>
2022-03-09 13:41:40 +0100

    feltöltés

--- tervAlapok_masodikOra/index.js
+++ tervAlapok_masodikOra/index.js
@@ -24,16 +24,40 @@ Mit akarok ezekkel az etelekkel?
 uj etel felvétele
 POST : /etel
 be is ki paraméterek
+  nev:
+  szint:
+
+  id
+  nev
+  szintet
+
 etelek listazasa
 GET : /etelek
+bemenet null
+ételek listája JSON formátum
+
+
 egy etel listazasa
 GET: /etel/:id
+null is lehet
+Egy étel JSON formátum
+
+
 etel torlese
 DELETE : /etel/:id
 etel keresese 
+a törölt étel json formában
+
+
 POST : /keres
+nev: érték vagy null
+szint: érték vagy null
+ételek listája
+
 etel modositasa
 PATCH : /etel/:id
+null
+módosított étel JSON-ben
 */
 
 /*
@@ -53,20 +77,50 @@ D -delete SQL: DELETE : (DELETE)
 
 */
 
+//3.ora
+/*
+lehet benne tobb metodus, azok segitenek majd
+nev hasznalat figyelése
 
-const express = require('express')
-const app = express()
-const port = 3000
+*/
+
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
 const { v4 : uuidv4 } = require('uuid');
 
 
-app.get('/', (req, res) => {
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); //mindig jsonben szeretnem latni
+
+
+
+app.get('/', (req, res, next) => {
   res.status(200).send(`helloo node id: ${uuidv4()}`);
 })
 
-app.get('etelek/:id', etelekListajaMw),
 
-app.post()
+
+const {initDatabase} = require('./services/db');
+
+const addRoute = require('./route/route');
+
+app.use(express.static('public')); //statikus root
+
+initDatabase( err, (db, myModel) => {
+  if(err){
+    return console.error(`DB error:${err}`); //későbbi fejlesztés esetén sem irunk utana ha ott a return
+  }
+
+  addRoute(app, db, myModel);
+})
+
+
+
+
 app.listen(port, () => {
   console.log(`Az alkalmazás portja: ${port}`)
 })
---
+++ tervAlapok_masodikOra/route/route.js
@@ -0,0 +1,29 @@
+const uuid = require('uuid');
+
+const etelekListajaMw = require('../middleWare/eteleklistaja');
+const etelMw = require('../middleWare/etel');
+const ujEtelMw = require('../middleWare/ujetel');
+const egyEtelTorleseMw = require('../middleWare/egyeteltorlese');
+const etelKeresMw = require('../middleWare/etelkeres');
+const etelModositasMw = require('../middleWare/etelmodositasa');
+
+function addroutes(app, db, myModel){
+    
+    const objRep = {
+        myModel,
+        db,
+        uuid
+    }
+
+    app.get('/etelek', etelekListajaMw(objRep));
+    app.get('/etel/:id', etelMw(objRep));
+    app.post('/etel', ujEtelMw(objRep));
+    app.delete('/etel/:id', etelMw(objRep), egyEtelTorleseMw(objRep));
+    app.post('/search', etelKeresMw(objRep));
+    app.patch('/etel/:id', etelMw(objRep), etelModositasMw(objRep));
+}
+
+
+
+module.exports = addroutes;
+
